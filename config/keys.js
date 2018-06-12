1a08bd47259a0081b365519679616bfe3f1848b5
Joe Gallegos <j.gallegos25u@gmail.com>
2018-06-12 12:12:50 -0500

    set up for prod push

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://joe:joe@ds127899.mlab.com:27899/devconnect',
-  secretOrKey: 'secret'
-};
+if (process.env.NOD_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
